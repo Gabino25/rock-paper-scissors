ef09db05dfc4a568fcc6d17a12508bddfac5a570
Gabino25 <gabino.hernandez.sab@gmail.com>
2020-11-22 17:18:59 -0600

    221120201718
    221120201718

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function computerPlay(){
+
+}
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+}
+
+const playerSelection = "";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+    
+}
